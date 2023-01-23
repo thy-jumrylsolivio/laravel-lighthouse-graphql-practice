8e00a4e7b973f91afaacd2e2428bffdd152ba1fa
Jumryl <thy.jumrylsolivio@gmail.com>
2023-01-23 18:05:53 +0800

    add_UI when fetching data

--- react-gql/src/pages/UserList.js
+++ react-gql/src/pages/UserList.js
@@ -1,26 +1,55 @@
-import { useEffect } from 'react';
+import React from 'react';
 import { useQuery, gql } from "@apollo/client"
 
 const GET_USER = gql`
 {
   usered{
-    name,
-    id,
+    name
+    id
     email
     
   }
 }
 `;
 
-export default function UserList() {
-  
-  const { error, loading, data } = useQuery(GET_USER);
-  useEffect(() => { 
-  
-    const intervalId =setInterval(() => {
-console.log({error, loading, data});
-    }, 10000);
-    return () => clearInterval(intervalId);
-  });
+export default function UserList(){
+  const { error, data, loading } = useQuery(GET_USER);
+
+  console.log({error, loading, data });
+
+  if (loading) return <div>Spinner ...</div>;
+
+  if (error) return <div>something went wrong</div>;
+
+
+  return (
+    <div className="ListUsers">
+      {data.usered.map((userChar) => {
+        return (
+          <div key={userChar.id}>
+            <h2>{userChar.name}</h2>
+          </div>
+        );
+      })}
+    </div>
+  );
+
 }
+
+
+
+
+
+
+// export default function UserList() {
+  
+//   const { error, loading, data } = useQuery(GET_USER);
+//   useEffect(() => { 
+  
+//     const intervalId =setInterval(() => {
+// console.log({error, loading, data});
+//     }, 100000);
+//     return () => clearInterval(intervalId);
+//   });
+// }
 
